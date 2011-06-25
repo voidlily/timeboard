6c8f06244b99fb0b411a1a8f00e0885b86452d6f
Lily <lily@voidlily.net>
2011-06-25 14:54:14 -0400

    Timesheet status validation

--- app/models/timesheet.rb
+++ app/models/timesheet.rb
@@ -1,3 +1,4 @@
 class Timesheet < ActiveRecord::Base
   belongs_to :student
+  has_many :timesheet_statuses
 end
--- app/models/timesheet_status.rb
+++ app/models/timesheet_status.rb
@@ -1,2 +1,12 @@
 class TimesheetStatus < ActiveRecord::Base
+  belongs_to :timesheet
+  scope :current, order("created_on DESC").limit(1)
+
+  #type_regex = /\A(?:(Draft)|(Signed)|(Approved)|(Processed)|(Disapproved))\Z/
+  types = %w(Draft Signed Approved Processed Disapproved)
+
+  validates :timesheet, :presence => true
+  validates :status, :presence => true,
+                     #:format => { :with => type_regex }
+                     :inclusion => types
 end
