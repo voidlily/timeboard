89b836d52aae4c310851432145fb1177365187ac
Bobby King <bobby@lappy.(none)>
2011-07-08 14:35:38 -0400

    Redirect to various different locations upon login.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,7 +6,22 @@ class PagesController < ApplicationController
   end
 
   def login
-    redirect_to root_path
+    #Redirects users to appropriate paths
+    user = User.find_by_account(session[:cas_user])
+    if (user.nil?)
+      redirect_to root_path
+      return
+    end
+    if user.type == "Student"
+      #TODO make this go to current timesheet.
+      redirect_to timesheets_path(:status => "Drafts")
+    elsif user.type == "Professor"
+      redirect_to timesheets_path(:status => "Signed")
+    elsif user.type == "Finance"
+      redirect_to timesheets_path(:status => "Approved")
+    else
+      redirect_to root_path
+    end
   end
 
   def logout
