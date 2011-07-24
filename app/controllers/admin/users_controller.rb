e574717bb1a74f0273ff0f01b3b5ba7e6a026629
Lily <lily@voidlily.net>
2011-07-23 22:59:36 -0400

    Initial css import controller

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,22 @@
+class Admin::UsersController < ApplicationController
+  def csv_import
+    # CSV schema:
+    # name,email,account,course_name
+    
+    # First, set all students to inactive
+    # This should be done with update_all for less queries
+    Student.all.each do |student|
+      student.active = false
+      student.save
+    end
+
+    # @file = ???
+    csv = FCSV.new(@file)
+    csv.each do |row|
+      student = Student.find_by_account(row[2]) || Student.new
+      course |= Course.find_by_name(row[3])
+      student.update_attributes(:name => row[0], :email => row[1], :account => row[2], :course => course, :active => true)
+      student.save
+    end
+  end
+end
